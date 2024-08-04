e70fee995e50e3b7bb2f7482a6cc88032a888d37
KIM-BEOMSOO <clever0305@naver.com>
2024-08-04 21:58:14 +0900

    폴더구조 생성 (controller/mapper 등)

--- src/main/java/com/example/myBatisBoard/controller/BoardController.java
+++ src/main/java/com/example/myBatisBoard/controller/BoardController.java
@@ -1,13 +1,14 @@
 package com.example.myBatisBoard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BoardController {
 
-    @RequestMapping("/board/boardMain")
+    @GetMapping("/board/boardMain")
     public String boardMain(){
         return "/board/boardMain";
     }
