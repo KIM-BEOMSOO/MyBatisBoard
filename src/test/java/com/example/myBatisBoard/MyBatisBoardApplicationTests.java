27e1bd270459008943466311634c327f410c3d4c
KIM-BEOMSOO <clever0305@naver.com>
2024-08-04 17:38:22 +0900

    MyBatis로 게시판 만들기

---
+++ src/test/java/com/example/myBatisBoard/MyBatisBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myBatisBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
