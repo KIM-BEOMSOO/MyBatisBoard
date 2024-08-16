98fef2eed4aa73b5e0df549ccf6e9571e78fc9cb
KIM-BEOMSOO <clever0305@naver.com>
2024-08-17 07:27:38 +0900

    impl 분리

--- src/main/java/com/example/myBatisBoard/service/board/guest/GuestBoardService.java
+++ src/main/java/com/example/myBatisBoard/service/board/guest/GuestBoardService.java
@@ -8,57 +8,32 @@ import org.springframework.stereotype.Service;
 import java.beans.Transient;
 import java.util.List;
 
-@Service
-public class GuestBoardService {
-    @Autowired
-    private GuestBoardRepository GuestBoardRepository;
+
+public interface GuestBoardService{
 
     /*Create*/
     /*비회원 게시글 쓰기*/
-    public void createGuestPost(GuestPost guestPost){
-        GuestBoardRepository.createGuestPost(guestPost);
-    }
+    public void createGuestPost(GuestPost guestPost);
 
     /*테스트용 비회원 게시글 생성*/
     @Transient
-    public void createMultipleGuestPost(){
-        for (int i = 1; i <= 100; i++) {
-            GuestPost post = new GuestPost(
-                    "User" + i,
-                    "Title " + i,
-                    "This is content for post " + i,
-                    "password" + i
-            );
-            GuestBoardRepository.createGuestPost(post);
-        }
-    }
+    public void createMultipleGuestPost();
 
     /*Read*/
     /*비회원 게시판 게시글 전체출력*/
-    public List<GuestPost> getAllGuestPost(int pageSize, int offset) {
-        return GuestBoardRepository.getAllGuestPost(pageSize, offset);
-    }
+    public List<GuestPost> getAllGuestPost(int pageSize, int offset);
 
     /*비회원 게시글 상세보기*/
-    public GuestPost getGuestPostById(Long id){
-        return GuestBoardRepository.getGuestPostById(id);
-    };
+    public GuestPost getGuestPostById(Long id);
 
     /*비회원 게시글 Count*/
-    public int getGuestPostCount() {
-        return GuestBoardRepository.getGuestPostCount();
-    }
+    public int getGuestPostCount();
 
     /*Update*/
     /*비회원 게시글 수정*/
-    public void updateGuestPost(GuestPost guestPost){
-        GuestBoardRepository.updateGuestPost(guestPost);
-    }
+    public void updateGuestPost(GuestPost guestPost);
 
     /*Delete*/
     /*비회원 게시글 삭제*/
-    public void deleteGuestPost(Long id){
-        GuestBoardRepository.deleteGuestPost(id);
-    }
-
+    public void deleteGuestPost(Long id);
 }
